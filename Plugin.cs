a38dffec5e010e7ff40dc206b57fb91d8c33e57c
YoBii <xschoppmeier+github@gmail.com>
2024-04-01 17:02:17 +0200

    rework event execution; LethalQuantities compatibility patch

--- Plugin.cs
+++ Plugin.cs
@@ -16,14 +16,10 @@ namespace HullBreakerCompany
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private static Dictionary<int, SelectableLevelState> _levelStates = new();
-
         private static bool _loaded;
         public static ManualLogSource Mls;
 
-        public static bool OneForAllIsActive;
-        public static bool BountyIsActive;
-        public static bool BountyFirstKill;
+        public static bool compatibilityLQ = false;
 
         public static int DaysPassed;
         public static float BunkerEnemyScale;
@@ -83,13 +79,22 @@ namespace HullBreakerCompany
 
         [HarmonyPatch(typeof(RoundManager), nameof(RoundManager.LoadNewLevel))]
         [HarmonyPrefix]
-        static bool ModifiedLoad(ref SelectableLevel newLevel)
-        {
-            CurrentEvents.Clear();
-            Mls.LogInfo("Client is host: " + RoundManager.Instance.IsHost);
+        [HarmonyPriority(50)] // make sure we come last
+        static bool ModifiedLoad(ref SelectableLevel newLevel) {
+            LQCompaitiblityPatch();
 
+            EventsManager.AddModEvents();
+
+            HullManager.LogBox("MODIFIED LEVEL LOAD");
+
+            //Quit if client
+            Mls.LogInfo("Client is host: " + RoundManager.Instance.IsHost);
             if (!RoundManager.Instance.IsHost) return true;
 
+            // Refresh weights and settings
+            Mls.LogInfo("Refreshing config..");
+            ConfigManager.RefreshConfig();
+            
             CustomEventLoader.DebugLoadCustomEvents();
             
             //Begin modified Load
@@ -104,132 +109,16 @@ namespace HullBreakerCompany
             //Event Execution
             EventsManager.ExecuteEvents(newLevel);
                     
-                    CurrentEvents.Add(hullEvent);
-                }
-                catch (NullReferenceException ex)
-                {
-                    Mls.LogError(
-                        $"NullReferenceException caught while processing event: {gameEvent}. Exception message: {ex.Message}. Caused : {ex.InnerException}");
-                }
-            }
-
-            if (EventCount != 0 && EnableEventMessages) //check if configs allows events
-            {
-                //count how many active events are NothingEvent
-                int nothingEvent_count = roundEvents.Where(e => e.Equals("Nothing")).Count();
-                Mls.LogInfo($"Round events: " + roundEvents.Count);
-                Mls.LogInfo($"Nothing events: " + nothingEvent_count);
-                
-                //Only print Notes to game chat when there's at least one Event that's not NothingEvent
-                if (roundEvents.Count > nothingEvent_count) {
-                    HullManager.AddChatEventMessage("<color=red>NOTES ABOUT MOON:</color>", true);
-                    HullManager.SendChatEventMessages();
-                } 
-            }
-            
-            //debug logs
-            HullManager.LogEnemyRarity(newLevel.Enemies, "\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b\u2b1bENEMIES RARITY\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b");
-            HullManager.LogEnemyRarity(newLevel.DaytimeEnemies, "\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b\u2b1bDAYTIME ENEMIES RARITY\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b");
-            HullManager.LogEnemyRarity(newLevel.OutsideEnemies, "\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b\u2b1bOUTSIDE ENEMIES RARITY\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b\u2b1b");
-            
-            
-            if (UseHullBreakerLevelSettings)
-            {
-                nl.maxEnemyPowerCount += 16;
-                nl.maxOutsideEnemyPowerCount += 20;
-
-                nl.daytimeEnemySpawnChanceThroughDay = new AnimationCurve(new Keyframe(0f, 5f), new Keyframe(0.5f, 5f));
-                nl.enemySpawnChanceThroughoutDay = new AnimationCurve(new Keyframe(0f, 256f));
-            }
-            else if (UseVanillaGameSettings)
-            {
-                Mls.LogInfo("Vanilla settings");
-            }
-            else
-            {
-                nl.maxEnemyPowerCount = MaxEnemyPowerCount;
-                nl.maxOutsideEnemyPowerCount = MaxOutsideEnemyPowerCount;
-                nl.maxDaytimeEnemyPowerCount = MaxDaytimeEnemyPowerCount;
-                nl.enemySpawnChanceThroughoutDay = new AnimationCurve(new Keyframe(0f, BunkerEnemyScale));
-            }
-
-            newLevel = nl;
-            CurrentMessage = "Events not found";
-            CurrentMessage = "<color=green>Notes about the <color=white>" + newLevel.PlanetName + ":\n\n";
-            foreach (var hullEvent in CurrentEvents)
-            {
-                CurrentMessage += "<color=orange>" + hullEvent.ID() + ": <color=white> " + hullEvent.GetMessage() + "\n\n";
-            }
-            
             return true;
         }
-
-        private static void UpdateRarity(List<SpawnableEnemyWithRarity> enemies, Dictionary<Type, int> componentRarity)
-        {
-            if (componentRarity.Count <= 0) return;
-
-            foreach (var unit in enemies)
-            {
-                foreach (var componentRarityPair in componentRarity)
-                {
-                    if (unit.enemyType.enemyPrefab.GetComponent(componentRarityPair.Key) == null)
-                        continue;
-                    if (enemies.Any(e => e.enemyType == unit.enemyType))
-                    {
-                        unit.rarity = componentRarityPair.Value;
-                        componentRarity.Remove(componentRarityPair.Key);
-                    }
-                    break;
+        private static void LQCompaitiblityPatch() {
+            if (!compatibilityLQ) {
+                if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey(LethalQuantities.PluginInfo.PLUGIN_GUID)) {
+                    Mls.LogInfo("Lethal Quantities found! Applying compatibility patch!");
+                    Harmony.CreateAndPatchAll(typeof(EventsManager));
+                    compatibilityLQ = true;
                 }
             }
         }
-        
-        public static void addLandminesToLevelUnits(SelectableLevel level, float overrideAmount = 0f)
-        {
-            float amount = LandmineScale;
-            if (overrideAmount > 0f)
-                amount = overrideAmount;
-            Mls.LogInfo($"Adding Landmines: {amount}");
-
-            foreach (var mapObject in level.spawnableMapObjects)
-            {
-                if ((UnityEngine.Object)(object)mapObject.prefabToSpawn.GetComponentInChildren<Landmine>() != null)
-                {
-                    mapObject.numberToSpawn = new AnimationCurve(new Keyframe(0f, amount));
-                }
-            }
-        }
-
-        public static void addTurretsToLevelUnits(SelectableLevel n, float overrideAmount = 0f)
-        {
-            float amount = TurretScale;
-            if (overrideAmount > 0f)
-                amount = overrideAmount;
-            Mls.LogInfo($"Adding Turrets: {amount}");
-            foreach (var mapObject in n.spawnableMapObjects)
-            {
-                if (mapObject.prefabToSpawn.GetComponentInChildren<Turret>() != null)
-                {
-                    mapObject.numberToSpawn = new AnimationCurve(new Keyframe(0f, amount));
-                }
-            }
-        }
-
-
-        private static void ResetLevelUnits(SelectableLevel level)
-        {
-            foreach (var mapObject in level.spawnableMapObjects)
-            {
-                if (mapObject.prefabToSpawn.GetComponentInChildren<Landmine>() != null)
-                {
-                    mapObject.numberToSpawn = new AnimationCurve(new Keyframe(0f, 2.5f));
-                }
-                if (mapObject.prefabToSpawn.GetComponentInChildren<Turret>() != null)
-                {
-                    mapObject.numberToSpawn = new AnimationCurve(new Keyframe(0f, 2.5f));
-                }
-            }
-        }
-
     }
 }
